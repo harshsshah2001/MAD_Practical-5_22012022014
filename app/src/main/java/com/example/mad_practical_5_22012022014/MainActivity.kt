693691febc356ac87acec963d12de296c93538ca
student <student@gnu.ac.in>
2023-08-18 12:26:26 +0530

    Commit

--- app/src/main/java/com/example/mad_practical_5_22012022014/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_5_22012022014/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.MediaStore
+import android.telecom.Call
 import android.widget.Button
 import android.widget.EditText
 
@@ -18,12 +22,24 @@ class MainActivity : AppCompatActivity() {
         val browseButton5:Button=findViewById(R.id.Btn_Browse5)
         val browseButton6:Button=findViewById(R.id.Btn_Browse6)
         val editTextBrowser:EditText=findViewById(R.id.editTextText)
-
+        val editTextBrowser2:EditText=findViewById(R.id.editTextText2)
         browseButton.setOnClickListener {
             OpenUrl(editTextBrowser.text.toString())
         }
-        browseButton4.setOnClickListener {
+        browseButton2.setOnClickListener {
+            Call(editTextBrowser2.text.toString())
+        }
+        browseButton3.setOnClickListener {
             CallLog()
+        }
+        browseButton4.setOnClickListener {
+            Gallery()
+        }
+        browseButton5.setOnClickListener {
+            Cemera()
+        }
+        browseButton6.setOnClickListener {
+            Alaram()
         }
     }
     fun OpenUrl(url:String){
@@ -33,15 +49,15 @@ class MainActivity : AppCompatActivity() {
         Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$phone")).also { startActivity((it))}
     }
     fun CallLog(){
-
+        Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
     }
     fun Gallery(){
-        Intent(Intent.CATEGORY_APP_GALLERY).also { startActivity(it) }
+        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity(it) }
     }
     fun Cemera(){
-
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
     }
     fun Alaram(){
-
+        Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
     }
 }
